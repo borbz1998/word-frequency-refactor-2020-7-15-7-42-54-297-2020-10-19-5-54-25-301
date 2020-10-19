d1634d1ba0b75a120a586071bceb72de2522f0a6
BORBECH <charlieborbs18@gmail.com>
2020-10-19 16:16:02 +0800

    [WordFrequencyGame][Charlie] refactor: Delete unused methods

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,10 +13,8 @@ public class WordFrequencyGame {
             try {
                 List<WordInfo> wordInfoList = getCalculateFrequency(sentence);
 
+                // Ascending Order
                 wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
-
-//                StringJoiner joiner = getStringJoiner(wordInfoList);
-//                return joiner.toString();
 
                 return getStringJoiner(wordInfoList);
 
@@ -24,7 +22,6 @@ public class WordFrequencyGame {
                 return "Calculate Error";
             }
         }
-
 
     }
 
@@ -38,26 +35,6 @@ public class WordFrequencyGame {
     }
 
     private List<WordInfo> getCalculateFrequency(String sentence) {
-//        //split the input string with 1 to n pieces of spaces
-//        String[] words = sentence.split(WHITE_SPACES);
-//
-//        List<WordInfo> wordInfoList = new ArrayList<>();
-//        for (String word : words) {
-//            WordInfo wordInfo = new WordInfo(word, 1);
-//            wordInfoList.add(wordInfo);
-//        }
-//
-//        //get the map for the next step of sizing the same words
-//        Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
-//
-//        List<WordInfo> distinctInfoWords = new ArrayList<>();
-//        for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
-//            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-//            distinctInfoWords.add(wordInfo);
-//        }
-//        wordInfoList = distinctInfoWords;
-//        return wordInfoList;
-
         List<String> words = Arrays.asList(sentence.split(WHITE_SPACES));
         List<WordInfo> wordInfoList = new ArrayList<>();
         for (String word : new HashSet<>(words)) {
@@ -66,22 +43,4 @@ public class WordFrequencyGame {
         }
         return wordInfoList;
     }
-
-
-    private Map<String, List<WordInfo>> getListMap(List<WordInfo> inputList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList words = new ArrayList<>();
-                words.add(input);
-                map.put(input.getValue(), words);
-            } else {
-                map.get(input.getValue()).add(input);
-            }
-        }
-        return map;
-    }
-
-
 }
